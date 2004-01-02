62a82160480595d3badfdc859f4f0c30c7d5bb5c
Tom Insam <tom@jerakeen.org>
2004-01-02 23:56:24 +0000

    *** empty log message ***

--- lib/URI/Title.pm
+++ lib/URI/Title.pm
@@ -2,12 +2,63 @@
 
 URI::Title - get the titles of things on the web in a sensible way
 
+=head1 SYNOPSIS
+
+  use URI::Title qw( title );
+  my $title = title('http://microsoft.com');
+  print "Title is $title\n";
+
+=head1 DESCRIPTION
+
+I keep having to find the title of things on the web. This seems like a really
+simple request, just get() the object, parse for a title tag, you're done. Ha,
+I wish. There are several problems with this approach:
+
+=over 4
+
+=item What if the resource is on a very slow server? Do we wait for ever or what?
+
+=item What if the resource is a 900 gig file? You don't want to download that.
+
+=item What if the page title isn't in a title tag, but is buried in the HTML somewhere?
+
+=item What if the resource is an MP3 file, or a word document or something?
+
+=item ...
+
+=back
+
+So, let's solve these issues once.
+
+=head1 METHODS
+
+only one, the title(url) method. Call it with an url, get the title if possible,
+undef if it wasn't. Very simple.
+
+=head1 TODO
+
+Many, many, many things. Still unimplemented:
+
+=over 4
+
+=item Get titles of MP3 files, Word Docs, PDFs, etc.
+
+=item Configurable.. well, anything, in fact. Timeout would be a good start.
+
+=item Better error reporting.
+
+=head1 CREDITS
+
+Invented because of a conversation with rjp, who contributed some eyeball-melting and
+as-yet-unused code to get titles from MP3s and PDFs, and hex, who has also solved the
+problem, and got bits done in a nicer way than I did.
+
 =cut
 
 package URI::Title;
 
 use base qw(Exporter);
-our @EXPORT = qw( title );
+our @EXPORT_OK = qw( title );
 
 our $VERSION = '0.1';
 
