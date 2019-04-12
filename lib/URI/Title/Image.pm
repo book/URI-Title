fa76c0122e7028c809f67f672f7d8968488ed3e1
Håkon Hægland <hakon.hagland@gmail.com>
2019-04-12 21:36:10 +0200

    Fixes Bug #57066 : add title from png metadata.
    To add the title of the image as embedded in the png file, we use
    either module Image::ExifTool or Image::PNG::Libpng. It is up
    to the user to install these libraries.

--- lib/URI/Title/Image.pm
+++ lib/URI/Title/Image.pm
@@ -13,14 +13,80 @@ sub types {(
   'image/x-png',
 )}
 
+sub got_exif_tool {
+  eval {
+    require Image::ExifTool;
+  };
+  return $@ ? 0 : 1;
+}
+
+sub got_lib_png {
+  eval {
+    require Image::PNG::Libpng;
+  };
+  return $@ ? 0 : 1;
+}
+
+sub pnginfo {
+  if (got_exif_tool()) {
+    return pnginfo_exif_tool( @_ );
+  }
+  else {
+    return pnginfo_lib_png( @_ );
+  }
+}
+
+sub pnginfo_exif_tool {
+  my ($data_ref) = @_;
+  my $title = "";
+  my $x = 0;
+  my $y = 0;
+  my $info = Image::ExifTool::ImageInfo($data_ref);
+  return ($info->{ImageWidth}, $info->{ImageHeight}, gen_title_str($info->{Title}));
+}
+
+sub gen_title_str {
+  return $_[0] ? " : " . $_[0] : "";
+}
+
+sub pnginfo_lib_png {
+  my ($data_ref) = @_;
+  my $title = "";
+  my $x = 0;
+  my $y = 0;
+  my $png = Image::PNG::Libpng::read_from_scalar($$data_ref);
+  $x = $png->get_image_width();
+  $y = $png->get_image_height();
+  my $text_chunks = $png->get_text();
+  for (@$text_chunks) {
+    if ($_->{key} eq "Title") {
+      $title = gen_title_str($_->{text});
+      last;
+    }
+  }
+  return ($x, $y, $title);
+}
+
+sub can_extract_png_title {
+  return got_lib_png() || got_exif_tool();
+}
+
 sub title {
   my ($class, $url, $data, $type) = @_;
 
-  my ($x, $y) = imgsize(\$data);
   $type =~ s!^[^/]*/!!;
   $type =~ s!^x-!!;
+  my $title = "";
+  my $x = 0;
+  my $y = 0;
+  if ( can_extract_png_title() && $type =~ /png/ ) {
+    ($x, $y, $title) = pnginfo(\$data);
+  }
+  else {
+    ($x, $y) = imgsize(\$data);
+  }
   return $type unless $x && $y;
-  return "$type ($x x $y)";
+  return "$type ($x x $y)$title";
 }
 
 1;
